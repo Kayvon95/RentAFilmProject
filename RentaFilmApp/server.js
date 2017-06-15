027b028de51132de3337e36b1a5dc9d0b2fb22d7
Kayvon Rahimi Morad Ali <krahimim@avans.nl>
2017-06-15 12:10:58 +0200

    Commentaar in Nederlands gezet
    Voor consistentie

--- RentaFilmApp/server.js
+++ RentaFilmApp/server.js
@@ -1,24 +1,25 @@
 /**
  * Created by Kayvon Rahimi on 13-6-2017.
  */
-//Initialize components.
+//Initialiseer de hoofdcomponenten van de server-applicatie.
 var http = require('http');
 var express = require('express');
 var config = require('./config.json');
 var bodyParser = require('body-parser');
 var routes_v1 = require('./routes/routes_v1');
 var auth_routes_v1 = require('./routes/authentication.routes.v1');
+var expressJWT = require('express-jwt');
+
 var db = require('./db/db_connector');
 
 var app = express();
 
-var expressJWT = require('express-jwt');
-
-// Bodyparser lets you use the body from a request, which contains the content of a post request
+// Bodyparser om informatie uit een post-request te kunnen verwerken in de logica van endpoints.
 app.use(bodyParser.urlencoded({ 'extended': 'true' }));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
 
+//Laat de server gebruik maken van JWT, haal de secret key op uit config.
 app.use(expressJWT({
     secret: config.secretkey
 }).unless({
@@ -32,12 +33,13 @@ app.set('PORT' , config.webPort);
 var port = process.env.PORT || app.get('PORT');
 
 
-//Log each endpoint URL when queried and the request-type.
+//Deze methode logt elke request type + url in de console.
 app.get( '*' , function(req, res, next){
     //example = GET + /hi, POST + /about
     console.log(  "Method type: " + req.method + " on URL:  " + req.url );
     next();
 });
+
 
 app.get('/about', function(request, response){
     response.send('This project serves as practice for programming classes.' + '\n'  +
